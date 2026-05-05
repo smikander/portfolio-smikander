fb99addfac5508ea4c93913505b5202ad86bab82
Sari Mikander <sarimikander@msn.com>
2026-05-05 10:46:32 +0300

    linkedin link

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <footer id="contact" className="contact">
       <h2>Get In Touch</h2>
       <p>Email: <a href="mailto:sarimikander@msn.com">sarimikander@msn.com</a></p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/sari-mikander-9b1a5b1a2/" target="_blank" rel="noopener noreferrer">linkedin.com/in/sari-mikander</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/sari-mikander-98369660/" target="_blank" rel="noopener noreferrer">linkedin.com/in/sari-mikander</a></p>
     </footer>
   );
 };
